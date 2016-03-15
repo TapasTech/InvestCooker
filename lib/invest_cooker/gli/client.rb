0ad1af9f8eaf71ea7dec521c51220dd892bfe8f8
lifeng <lithium4010@163.com>
2016-03-15 13:56:25 +0800

    优化GLI::Client使能在invest项目中通用

--- lib/invest_cooker/gli/client.rb
+++ lib/invest_cooker/gli/client.rb
@@ -1,9 +1,46 @@
 module InvestCooker
   module GLI
     class Client
-      def initialize(time=Time.now)
+
+      # 目前项目的时间有两种
+      # Rails 中使用 Time.zone.now
+      # Invest 的非 Rails 项目使用 Application.time_zone.now
+      def self.time_zone_for_rails_or_application
+        return Time.zone             if const_defined?('Rails')
+        return Application.time_zone if const_defined?('Application')
+        Time
+      end
+
+      TIME_ZONE = time_zone_for_rails_or_application
+
+      def initialize(time=TIME_ZONE.now)
         @folder = time.strftime('%Y%m%d')
+        @source_path = File.join(Settings.glidata.source_path, @folder)
         @target_path = File.join(Settings.glidata.target_path, @folder)
+      end
+
+      def date
+        @date ||= TIME_ZONE.parse(@folder)
+      end
+
+      # 根据文件名读取文件的第一行为字符串，并转换成 UTF-8
+      # 因为聚源的 json 文件只有一行，所以没有问题
+      def read(file_name)
+        connection do |sftp|
+          line = ''
+          result =
+            sftp.file.open(File.join(@source_path, file_name), "r") do |f|
+              line = f.gets
+            end
+          line
+        end.force_encoding("GB18030").encode("UTF-8")
+      end
+
+      # 读取当天文件夹下所有 js 文件名
+      def file_names
+        connection do |sftp|
+          sftp.dir.entries(@source_path).map(&:name)
+        end.select { |file_name| file_name =~ /\.js$/ }
       end
 
       def write(file_name, file_str, valid=true)
@@ -25,7 +62,9 @@ module InvestCooker
       private
 
       def connection(&block)
-        $gli_sftp_pool.with { |sftp| block.call(sftp) }
+        result = nil
+        $gli_sftp_pool.with { |sftp| result = block.call(sftp) }
+        result
       end
     end
   end
