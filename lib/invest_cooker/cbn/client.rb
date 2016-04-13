d026d32c234cbf504ba4ceb88930a5f2e1cbc046
lifeng <lithium4010@163.com>
2016-04-13 18:23:46 +0800

    提供筛选文件的功能

--- lib/invest_cooker/cbn/client.rb
+++ lib/invest_cooker/cbn/client.rb
@@ -1,8 +1,9 @@
 module InvestCooker
   module CBN
     class Client
+
       def initialize(time=TIME_ZONE.call.now)
-        @folder = time.strftime('%Y%m%d')
+        @folder = time.strftime('%Y%m%d'.freeze)
         @source = File.join(Settings.glidata.target_path, @folder)
       end
 
@@ -10,33 +11,38 @@ module InvestCooker
       # 因为聚源的 json 文件只有一行，所以没有问题
       # @return hash_of_json_file
       def read(file_name)
-        line = ''
-        connection do |sftp|
-          sftp.file.open(File.join(@source, file_name), "r") do |f|
-            line = f.gets
+        file_str =
+          connection do |sftp|
+            sftp.file.open(file_path(file_name), "r") { |f| f.gets }
           end
-        end
-        file_str = line
         Oj.load file_str, symbol_keys: true
       end
 
+      REGEXP_JSON_FILE = /\.json$/.freeze
+
       # 读取当天文件夹下所有 json 文件名
       # @return list_of_file_names
-      def list(page, per)
-        list = []
-        connection do |sftp|
-          list = sftp.dir.entries(@source)
-        end
-        list = list.select { |file| file.name =~ /\.json$/ }
-        list = list.sort_by { |file| file.attributes.mtime }.reverse
+      def list(page=nil, per=nil, &filter=->(_) { true })
+        files =
+          connection { |sftp| sftp.dir.entries(@source) }
+            .select { |file| file.name =~ REGEXP_JSON_FILE }
+            .select(&filter)
+            .sort_by { |file| file.attributes.mtime }
+            .reverse
 
-        Kaminari.paginate_array(list).page(page).per(per)
+        Kaminari.paginate_array(files).page(page).per(per)
       end
 
       private
 
+      def file_path(file_name)
+        File.join(@source, file_name)
+      end
+
       def connection(&block)
-        $gli_sftp_pool.with { |sftp| block.call(sftp) }
+        result = nil
+        $gli_sftp_pool.with { |sftp| result = block.call(sftp) }
+        result
       end
     end
   end
