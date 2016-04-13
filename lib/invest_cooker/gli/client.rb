7fe9b9eb1c5da780408ac6eb54cb82fa2eb692cb
lifeng <lithium4010@163.com>
2016-04-13 18:24:10 +0800

    refactor

--- lib/invest_cooker/gli/client.rb
+++ lib/invest_cooker/gli/client.rb
@@ -2,7 +2,7 @@ module InvestCooker
   module GLI
     class Client
       def initialize(time=TIME_ZONE.call.now)
-        @folder = time.strftime('%Y%m%d')
+        @folder = time.strftime('%Y%m%d'.freeze)
         @source_path = File.join(Settings.glidata.source_path, @folder)
         @target_path = File.join(Settings.glidata.target_path, @folder)
       end
@@ -15,29 +15,30 @@ module InvestCooker
       # 因为聚源的 json 文件只有一行，所以没有问题
       def read(file_name)
         connection do |sftp|
-          sftp.file.open(File.join(@source_path, file_name), "r") { |f| f.gets }
-        end.force_encoding("GB18030").encode("UTF-8")
+          sftp.file.open(File.join(@source_path, file_name), 'r'.freeze) { |f| f.gets }
+        end.force_encoding("GB18030".freeze).encode("UTF-8".freeze)
       end
+
+      REGEXP_JS_FILE = /\.js$/.freeze
 
       # 读取当天文件夹下所有 js 文件名
       def file_names
-        connection do |sftp|
-          sftp.dir.entries(@source_path).map(&:name)
-        end.select { |file_name| file_name =~ /\.js$/ }
+        connection { |sftp| sftp.dir.entries(@source_path).map(&:name) }
+          .select { |file_name| file_name =~ REGEXP_JS_FILE }
       end
+
+      MKDIR = -> (sftp, path) { Try { sftp.mkdir! path } }.freeze
 
       def write(file_name, file_str, valid=true)
 
-        mkdir = -> (sftp, path) { Try { sftp.mkdir! path } }
-
         connection do |sftp|
           # 创建相关文件夹
-          mkdir.(sftp, path = @target_path)
-          mkdir.(sftp, path = File.join(path, 'invalid_incoming')) unless valid
+          MKDIR.(sftp, path = @target_path)
+          MKDIR.(sftp, path = File.join(path, 'invalid_incoming'.freeze)) unless valid
 
           # 写入文件
-          sftp.file.open(File.join(path, file_name), 'w') do |f|
-            f.puts file_str.force_encoding("ASCII-8BIT")
+          sftp.file.open(File.join(path, file_name), 'w'.freeze) do |f|
+            f.puts file_str.force_encoding('ASCII-8BIT'.freeze)
           end
         end
       end
