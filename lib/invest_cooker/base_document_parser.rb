3075ac5d984cf0868d66c9f6411ed7c65e09a626
lifeng <lithium4010@163.com>
2016-05-11 13:01:16 +0800

    fix bug

--- lib/invest_cooker/base_document_parser.rb
+++ lib/invest_cooker/base_document_parser.rb
@@ -2,14 +2,15 @@ class BaseDocumentParser
   class << self
 
     def dump(document, options={})
-      # 特殊情况不用全部 dump
-      hash = @skip_dump && document.instance_exec(options, &@skip_dump) || evaluate_result(document, options)
-
-      # dump 前处理 document
-      @before_dumps.to_a.each { |block| evaluate_considering_block_arity(document, options, block) }
-
-      # dump 后处理结果
-      @after_dumps.to_a.reduce(hash) { |result, block| evaluate_considering_block_arity(document, result, block) }
+      if @skip_dump.present?
+        document.instance_exec(options, &@skip_dump)
+      else
+        # dump 前处理 document
+        @before_dumps.to_a.each { |block| evaluate_considering_block_arity(document, options, block) }
+        @hash = evaluate_result(document, options)
+        # dump 后处理结果
+        @after_dumps.to_a.reduce(@hash) { |result, block| evaluate_considering_block_arity(document, result, block) }
+      end
     end
 
     def skip_dump(&block)
