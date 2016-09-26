4a3c62312d98c648d54374c9ad80b486abe7b15b
lifeng <lithium4010@163.com>
2016-09-26 12:10:11 +0800

    fix bug

--- lib/monkey_patches/pinyin.rb
+++ lib/monkey_patches/pinyin.rb
@@ -1,7 +1,9 @@
 module PinYin
   class << self
-    of_string_with_replace_v = Module.new do      
-      super(*args, &block).map { |str| str.gsub("\u0308u", 'v') }
+    of_string_with_replace_v = Module.new do
+      def of_string(*args, &block)
+        super(*args, &block).map { |str| str.gsub("\u0308u", 'v') }
+      end
     end
 
     prepend of_string_with_replace_v
