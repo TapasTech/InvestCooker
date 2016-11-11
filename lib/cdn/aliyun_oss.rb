7fe693b2d6ede8f9ee97ebc4b984e0c102adbfc3
lifeng <lithium4010@163.com>
2016-11-11 13:53:20 +0800

    图片上传阿里云 invest-images

---
+++ lib/cdn/aliyun_oss.rb
@@ -0,0 +1,23 @@
+require 'aliyun/oss'
+
+# 上传阿里云图片
+module CDN
+  class AliyunOSS
+    include Singleton
+
+    ACCESS_KEY = ENV['HUGO_INVEST_SERVER_ALIYUN_OSS_ACCESS_KEY'].freeze
+    SECRET_KEY = ENV['HUGO_INVEST_SERVER_ALIYUN_OSS_SECRET_KEY'].freeze
+    ENDPOINT   = ENV['ALIYUN_OSS_BUCKET_INVEST_IMAGE_URL'].freeze
+
+    def initialize
+      @client = ::Aliyun::OSS::Client.new endpoint: ENDPOINT,
+                                          access_key_id: ACCESS_KEY,
+                                          access_key_secret: SECRET_KEY,
+                                          cname: true
+    end
+
+    def upload(file_name, file_path)
+      @client.get_bucket('invest-images').put_object(file_name, file: file_path)
+    end
+  end
+end
