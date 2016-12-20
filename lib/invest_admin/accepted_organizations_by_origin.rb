0445829eae602428e9a52b45a1eb483c046eb8a3
lifeng <lithium4010@163.com>
2016-12-20 12:27:54 +0800

    add accepted_organizations_by_origin

---
+++ lib/invest_admin/accepted_organizations_by_origin.rb
@@ -0,0 +1,10 @@
+# 记录某一来源的稿件只接收来自某一类撰写机构的稿件
+module InvestAdmin
+  class AcceptedOrganizationsByOrigin
+    include Mongoid::Document
+    include Mongoid::Timestamps
+
+    field :origin_website, type: String
+    field :compose_organizations, type: Array, default: -> { [] }
+  end
+end
