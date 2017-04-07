ccc595bf123a0195474497b0db0be3fc4e52733e
lifeng <lithium4010@163.com>
2017-04-07 14:59:31 +0800

    feat: hola v1

---
+++ lib/hola.rb
@@ -0,0 +1,24 @@
+# 基于 Redis 的服务注册与发现
+class Hola
+
+  class << self
+
+    def self.key_template
+      'hola/services/%s'
+    end
+
+    # 将服务注册到 Redis
+    def register(service, ip_port_list)
+      key = key_template % service
+      $redis_object.zadd(key % service, *ip_port_list)
+    end
+
+    # 获得服务内网 ip:port
+    def fetch(service)
+      key = key_template % service
+      member = $redis_object.zrange(key, 0, 0).first
+      $redis_object.zincrby(key, 1, member)
+      member
+    end
+  end
+end
