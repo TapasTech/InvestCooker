c1433a9ff4558f8ea1b304d940c189742c3afdd6
lifeng <lithium4010@163.com>
2017-04-07 15:37:47 +0800

    feat: hola v3

--- lib/hola.rb
+++ lib/hola.rb
@@ -1,25 +1,28 @@
 # 基于 Redis 的服务注册与发现
 class Hola
+  attr_accessor :key, :service, :redis
 
-  class << self
+  def initialize(service)
+  	self.service = service
+    self.key = "hola/services/#{service}"
+    self.redis = $redis_object
+  end
 
-    def key_template
-      'hola/services/%s'
-    end
+  # 将服务注册到 Redis
+  def add(ip_port_list)
+    set = ip_port_list.map { |ip_port| [0, ip_port] }
+    redis.zadd(key % service, *set)
+  end
 
-    # 将服务注册到 Redis
-    def register(service, ip_port_list)
-      key = key_template % service
-      set = ip_port_list.map { |ip_port| [0, ip_port] }
-      $redis_object.zadd(key % service, *set)
-    end
+  # 获得服务内网 ip:port
+  def fetch
+    member = redis.zrange(key, 0, 0).first
+    redis.zincrby(key, 1, member)
+    member
+  end
 
-    # 获得服务内网 ip:port
-    def fetch(service)
-      key = key_template % service
-      member = $redis_object.zrange(key, 0, 0).first
-      $redis_object.zincrby(key, 1, member)
-      member
-    end
+  # 清空服务
+  def clear!
+    redis.del(key)
   end
 end
