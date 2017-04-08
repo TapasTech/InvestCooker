74eb7b70c64ab474979fcf67a6cd2d9ecf49be5f
lifeng <lithium4010@163.com>
2017-04-08 13:16:14 +0800

    feat: hola

--- lib/hola.rb
+++ lib/hola.rb
@@ -1,37 +1,71 @@
 # 基于 Redis 的服务注册与发现
 class Hola
-  attr_accessor :key, :service, :redis
+  attr_accessor :key, :redis, :service
 
   def initialize(service)
-  	self.service = service
+    self.service = service
     self.key = "hola/services/#{service}"
     self.redis = $redis_object
   end
 
   # 将服务注册到 Redis
-  def add(ip_port_list)
-    list = ip_port_list.select { |ip_port| ip_port.present? }
-    return unless list.present?
+  def add(hosts)
+    hosts = hosts.select(&:present?)
+    return unless hosts.present?
 
-    set = list.map { |ip_port| [0, ip_port] }
-    redis.zadd(key, *set)
+    hosts = hosts.map { |ip_port| [0, ip_port] }
+    redis.zadd(key, *hosts)
   end
 
   # 获得服务内网 ip:port
+  #   - 每调用一次该地址入队尾
+  #   - 地址连续失效多次移除该地址
   def fetch
-    host = redis.zrange(key, 0, 0).first
-    return unless host.present?
-    RestClient.get(host) # health check
-    redis.zincrby(key, 1, host)
-    host
-  rescue
-    redis.zrem(key, host)
-    retry
+    while true
+      host = redis.zrange(key, 0, 0).first
+      break host if host.blank?
+
+      redis.zincrby(key, 1, host)
+
+      pod = Pod.new(key, host, redis)
+      break host if pod.healthy?
+
+      redis.zrem(key, host) if pod.dead?
+    end
+  end
+
+  # 获得当前全部可用服务
+  def list
+    redis.zrange(key, 0, -1).select { |host| Pod.new(key, host, redis).healthy? }
   end
 
   # 清空服务
   def clear!
     redis.del(key)
+  end
+
+  class Pod
+    attr_accessor :key, :redis, :host, :max_fail
+
+    def initialize(service, host, redis)
+      self.key = "#{service}/#{host}/fail"
+      self.host = host
+      self.redis = redis
+      self.max_fail = 10
+    end
+
+    def healthy?
+      RestClient.get(host)
+      redis.del(key)
+      true
+    rescue
+      redis.incr(key, 1)
+      false
+    end
+
+    def dead?
+      redis.get(key).to_i > max_fail
+    end
   end
 
   class << self
@@ -40,6 +74,25 @@ class Hola
       return unless set.present?
 
       Hola.new(service).add(set)
+    end
+
+    # 初始化可用连接到内存
+    # 开新线程定时更新可用连接
+    def init(service)
+      $__hola_sync__ ||= {}
+      $__hola_host__ ||= {}
+
+      $__hola_sync__[service] ||= Thread.new do
+        while true
+          $__hola_host__[service] = new(service).fetch
+
+          sleep 10.seconds
+        end
+      end
+    end
+
+    def fetch(service)
+      $__hola_host__[service]
     end
 
     def ip
