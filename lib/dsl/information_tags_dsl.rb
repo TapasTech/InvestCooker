9cab3a128ff5c2c814f85983e86c04f079bdcb55
lifeng <lithium4010@163.com>
2017-07-05 10:43:45 +0800

    feat: information tags merge

--- lib/dsl/information_tags_dsl.rb
+++ lib/dsl/information_tags_dsl.rb
@@ -1,6 +1,7 @@
 concern :InformationTagsDSL do
   included do
     class << self
+      # 提供一个方法判断是否应该标记 information_tag
       def information_tag(name, &block)
         @information_tags ||= {}
         @information_tags[name] = block
@@ -9,14 +10,27 @@ concern :InformationTagsDSL do
       def information_tags
         @information_tags
       end
+
+      # 提供一个方法加入一组 informantion_tags
+      def information_tag_merge(&block)
+        @information_tag_merge ||= []
+        @information_tag_merge << block
+        @information_tag_merge
+      end
     end
 
     def information_tags
       tags = []
+
+      self.class.information_tag_merge.each do |block|
+        tags += block.call(self)
+      end
+
       self.class.information_tags.each do |name, block|
         tags << name if block.call(self)
       end
-      tags
+
+      tags.uniq
     end
 
     def information_tags_include?(name)
