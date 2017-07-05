7e3d52bf54b0963dcdc9dba1985434426d697b97
lifeng <lithium4010@163.com>
2017-07-05 11:27:49 +0800

    feat: search filter add type

--- lib/dsl/search_filter_dsl.rb
+++ lib/dsl/search_filter_dsl.rb
@@ -23,22 +23,32 @@ concern :SearchFilterDSL do
     class_exec(&block)
   end
 
+  # 数据的集合
   def collection(&block)
     @filters[@this_filter][:collection] = block
   end
 
+  # 展示的值
   def title(&block)
     @filters[@this_filter][:title_block] = block
   end
 
+  # 回调的值
   def value(&block)
     @filters[@this_filter][:value_block] = block
   end
 
+  # 排序
   def sort(&block)
     @filters[@this_filter][:sort_block] = block
   end
 
+  # 筛选框的类型
+  def type(&block)
+    @filters[@this_filter][:type_block] = block
+  end
+
+  # 附带描述信息
   def description(&block)
     @filters[@this_filter][:description_block] = block
   end
