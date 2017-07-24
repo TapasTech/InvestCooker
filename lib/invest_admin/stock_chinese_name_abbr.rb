1c3a98de241ca31f0745bba51ba723ed0287625e
lifeng <lithium4010@163.com>
2017-07-24 14:07:28 +0800

    feat: 股票中文名简称支持股票英文名

--- lib/invest_admin/stock_chinese_name_abbr.rb
+++ lib/invest_admin/stock_chinese_name_abbr.rb
@@ -1,8 +1,13 @@
+# 股票中文名简称
+# 股票英文名
 module InvestAdmin
   class StockChineseNameAbbr
     include Mongoid::Document
 
     field :chinese_name_abbr, type: String
-    field :code             , type: String
+    field :english_name,      type: String
+    field :code,              type: String
+
+    index code: 1
   end
 end
