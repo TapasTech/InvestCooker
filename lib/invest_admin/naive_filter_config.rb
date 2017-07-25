424ba140357e87b526651a18f79ee127e532938e
lifeng <lithium4010@163.com>
2017-07-25 15:56:08 +0800

    feat: 简单筛选项配置
    提供在 InvestAdmin 中配置简单筛选项的模型

---
+++ lib/invest_admin/naive_filter_config.rb
@@ -0,0 +1,21 @@
+# 简单筛选项配置
+module InvestAdmin
+  class NaiveFilterConfig
+    include Mongoid::Document
+    include Mongoid::Timestamps
+
+    field :title, type: String
+    field :value, type: String
+
+    belongs_to :team, index: true
+    scope :team_scope, ->(team) { where(team: team) }
+
+    def self.to_filter
+      self.all.map(&:to_filter)
+    end
+
+    def to_filter
+      {title: title, value: value}
+    end
+  end
+end
