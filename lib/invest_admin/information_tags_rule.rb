a31a2133980141ef7ab4409f9db79bcf77aa6499
lifeng <lithium4010@163.com>
2017-08-24 12:05:13 +0800

    fixup! feat: invest_admin/information_tags_rule

--- lib/invest_admin/information_tags_rule.rb
+++ lib/invest_admin/information_tags_rule.rb
@@ -7,9 +7,5 @@ module InvestAdmin
     field :name, type: String
     field :value, type: String
     field :target, type: String
-
-    def self.to_rule
-      self.to_a.map { |r| [r.name, r.value].join(':') }.join("\n")
-    end
   end
 end
