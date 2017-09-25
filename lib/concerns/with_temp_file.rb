b835e95ff6e28f56a156c915f35b8db10d1b4747
lifeng <lithium4010@163.com>
2017-09-25 16:31:40 +0800

    opt: with temp file work with non rails app

--- lib/concerns/with_temp_file.rb
+++ lib/concerns/with_temp_file.rb
@@ -2,7 +2,7 @@ module WithTempFile
   extend ActiveSupport::Concern
 
   def with_temp_file(data)
-    __file_path__ = File.expand_path("tmp/#{Digest::SHA1.hexdigest(data)}", Rails.root)
+    __file_path__ = "/tmp/#{Digest::SHA1.hexdigest(data)}"
 
     File.open(__file_path__, 'w+') { |f| f.puts data }
 
