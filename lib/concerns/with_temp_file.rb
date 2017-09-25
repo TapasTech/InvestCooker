e0096b5b514491601527d93e4169d399dbd6a8d2
lifeng <lithium4010@163.com>
2017-09-25 16:15:21 +0800

    opt: with temp file using in app without Time.zone.now

--- lib/concerns/with_temp_file.rb
+++ lib/concerns/with_temp_file.rb
@@ -2,7 +2,7 @@ module WithTempFile
   extend ActiveSupport::Concern
 
   def with_temp_file(data)
-    __file_path__ = File.expand_path("tmp/#{Digest::SHA1.hexdigest(data)}_#{Time.zone.now.to_i}", Rails.root)
+    __file_path__ = File.expand_path("tmp/#{Digest::SHA1.hexdigest(data)}", Rails.root)
 
     File.open(__file_path__, 'w+') { |f| f.puts data }
 
