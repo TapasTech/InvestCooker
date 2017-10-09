bb36b32f9e423f8772edb8768584386e8ac004f7
lifeng <lithium4010@163.com>
2017-10-09 18:02:57 +0800

    adapt for redis-rb 4.0.1

--- lib/utils/cache.rb
+++ lib/utils/cache.rb
@@ -1,7 +1,17 @@
 module Utils
   class Cache
     def self.redis
-      @redis ||= ActiveSupport::Cache::RedisStore.new($redis_cache.client.options)
+      @redis ||= ActiveSupport::Cache::RedisStore.new(client_info.options)
+    end
+
+    # REF: https://github.com/redis/redis-rb/blob/master/CHANGELOG.md#40
+    def self.client_info
+      @client_info ||=
+        if Redis::VERSION.split('.').first.to_i > 3
+          $redis_cache._client
+        else
+          $redis_cache.client
+        end
     end
 
     def self.redis_batch_delete(key)
