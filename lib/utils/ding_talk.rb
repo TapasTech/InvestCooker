b4ad8f3af29cfe70b311f3359f048fbfbd24b92b
lifeng <lithium4010@163.com>
2017-12-27 15:56:56 +0800

    feat: utils dingtalk

---
+++ lib/utils/ding_talk.rb
@@ -0,0 +1,31 @@
+module Utils
+  class DingTalk
+
+    def self.send_markdown_message(url:, title:, text:)
+      Service.new(url).send_markdown_message(title:, text:)
+    end
+
+    class Service
+      attr_accessor :url
+
+      def initialize(url)
+        self.url = url
+      end
+
+      def send_markdown_message(title:, text:)
+        post({
+          msgtype: 'markdown',
+          markdown: {
+            title: title,
+            text: text
+          }
+        })
+      end
+
+      def post(hash)
+        data = Oj.dump(hash.as_json)
+        RestClient::Resource.new(url, verify_ssl: false).post(data, {content_type: :json})
+      end
+    end
+  end
+end
