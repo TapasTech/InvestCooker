7f84a239dfa77dc97a2979f99f62b8a99f202fbc
li-thy-um <4740349+li-thy-um@users.noreply.github.com>
2019-12-03 14:47:56 +0800

    feat: add field :targets to content tags

--- lib/invest_admin/content_tag.rb
+++ lib/invest_admin/content_tag.rb
@@ -7,5 +7,6 @@ module InvestAdmin
     field :name, type: String
     field :parent, type: String
     field :source, type: String # 增同名，改 source，删同名，看 source
+    field :targets, type: Array # 标记分发到不同的使用场景
   end
 end
